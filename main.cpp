2b8d896e027161cd90c2047e1fada7e76a6a81fd
mahshid <zahra.oveisii@gmail.com>
2021-02-24 21:47:40 +0330

    changed texts language to persian

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QFontDatabase>
+#pragma execution_character_set("utf-8")
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFontDatabase::addApplicationFont("C:/Users/pc/Desktop/finalp3/GreenHouse/aviny.ttf");
     MainWindow w;
     w.show();
 
